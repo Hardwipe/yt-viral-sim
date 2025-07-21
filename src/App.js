a6de4d2dc81ffa4986f25245e92601104de43af7
Liam O'Brien <liamobr43@gmail.com>
2025-07-21 02:33:41 -0400

    Add vercel analytics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useRef } from 'react';
 import ViralDashboard from './components/ViralDashboard';
 import './App.css';
+import { Analytics } from "@vercel/analytics/react"
 
 
 function App() {
@@ -44,6 +45,7 @@ function App() {
         <p className="tagline">World’s Most Realistic Viral Simulator</p>
       </header>
 
+      <Analytics />
       {!started ? (
         <div className="upload-card">
           <input
